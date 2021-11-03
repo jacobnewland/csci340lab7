eeb572ab3ff60f011787d59f737a04205ef828a3
jacobnewland <89615158+jacobnewland@users.noreply.github.com>
2021-11-02 19:43:29 -0500

    Added Validation Error UI

--- csci340lab7/csci340lab7/Models/Trail.cs
+++ csci340lab7/csci340lab7/Models/Trail.cs
@@ -10,12 +10,19 @@ namespace csci340lab7.Models
     public class Trail
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
-
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Location { get; set; }
 
         [Display(Name = "Length in Miles")]
+        [Required]
         public decimal Length { get; set; }
+
+        [Display(Name = "1-10 Difficulty")]
+        [Required]
         public decimal Difficulty { get; set; }
     }
 }
